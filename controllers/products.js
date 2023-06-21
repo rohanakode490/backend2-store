676cc69a6530031fabf9df2b07d32c7f7bcb7d12
Rohan <rohanakode52@gmail.com>
2023-06-22 01:32:20 +0530

    improve finding name + sorting + select

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,22 @@
 const Product = require("../models/product");
 
 const getALLProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "vase table",
-  });
+  const search = "z";
+  // const products = await Product.find({
+  //   name: {$regex:search, $options:'i'}, //i is for case insensitive
+  // });
+
+  // SORT property
+  // const products = await Product.find({}).sort("-name price");
+  
+  // SELECT property 
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  // names are given by programmer 
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -18,10 +26,26 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
   if (name) {
-    queryObject.name = name;
+    queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(',').join(" ");
+    result = result.sort(sortList)
+  }
+  else{
+    result = result.sort('createdAt')
+  }
+
+  if(fields){
+    const fieldsList = fields.split(',').join(" ");
+    result = result.select(fieldsList)
+  }
+
+  const products = await result
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
