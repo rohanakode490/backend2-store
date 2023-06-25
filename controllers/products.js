0918405d85641b23a86d3fad08e7845e85344618
Rohan <rohanakode52@gmail.com>
2023-06-25 17:44:19 +0530

    limit + skip + numericFilters commit

--- controllers/products.js
+++ controllers/products.js
@@ -8,17 +8,27 @@ const getALLProductsStatic = async (req, res) => {
 
   // SORT property
   // const products = await Product.find({}).sort("-name price");
-  
-  // SELECT property 
-  const products = await Product.find({}).select("name price");
+
+  // SELECT property
+  // const products = await Product.find({})
+  //   .sort("name")
+  //   .select("name price")
+  //   .limit(4)
+  //   .skip(1);
+
+  // Numeric property
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  // names are given by programmer 
-  const { featured, company, name, sort, fields } = req.query;
+  // names are given by programmer
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured == "true" ? true : false;
   }
@@ -28,23 +38,52 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((items) => {
+      const [field, operator, value] = items.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   // console.log(queryObject);
   let result = Product.find(queryObject);
 
   if (sort) {
-    const sortList = sort.split(',').join(" ");
-    result = result.sort(sortList)
-  }
-  else{
-    result = result.sort('createdAt')
-  }
-
-  if(fields){
-    const fieldsList = fields.split(',').join(" ");
-    result = result.select(fieldsList)
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
 
-  const products = await result
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
 };
